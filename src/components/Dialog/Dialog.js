7ab0ba59bf2c52dba00f212f3bb71fa1c8f17292
nami <warriorparovoz@gmail.com>
2023-03-24 12:13:03 +0300

    dialog (font, layout) completed. Started work under the text and choices in dialog

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -5,7 +5,26 @@ import './Dialog.css';
 const Dialog = () => {
   return (
     <div className='dialog'>
-      Dialog
+      <div className='dialog__character'>
+        {/* Image of first character who talks */}
+      </div>
+      <div className='dialog__content'>
+        <p className='dialog__text'>
+          Здесь будет диалог с персонажем. Здесь будет диалог с персонажем. Здесь будет диалог с персонажем. 
+          Здесь будет диалог с персонажем. Здесь будет диалог с персонажем. Здесь будет диалог с персонажем. 
+          Здесь будет диалог с персонажем. Здесь будет диалог с персонажем. Здесь будет диалог с персонажем. 
+          Здесь будет диалог с персонажем. Здесь будет диалог с персонажем. Здесь будет диалог с персонажем. 
+          Здесь будет диалог с персонажем. Здесь будет диалог с персонажем. Здесь будет диалог с персонажем. 
+        </p>
+        <ul className='dialog__choices'>
+          <li className='dialog__choice'>
+            Завершить разговор
+          </li>
+        </ul>
+      </div>
+      <div className='dialog__character'>
+        {/* Image of second character who talks */}
+      </div>
     </div>
   )
 };

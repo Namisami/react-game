cc3ecd4384a7b424f11420b4a8b66107bbd09cf4
nami <warriorparovoz@gmail.com>
2023-04-16 17:08:15 +0300

    player logic moved from map component to player component

--- src/components/Characters/Player/Player.tsx
+++ src/components/Characters/Player/Player.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Character from '@components/Characters/Character/Character';
 import { Position } from '@config/types/Position'
+import { symbolSize } from '@config/variables/variables';
+import { moveKeys } from '@config/variables/moveKeys';
+import { gameMap } from '@utils/loadMap';
+import { gameNpcs } from '@utils/loadNpcs';
+import { getAbsolutePosition } from '@utils/getAbsolutePosition';
+
+import { positionChange } from '@store/slices/userSlice'
 
 interface PlayerProps {
   position: Position;
   isBusy: boolean;
-  onPlayerMove: ({x, y}: Position) => void;
   onInteract: () => void;
   onAttack: (isAttack: boolean, {x, y}: Position) => void;
 }
@@ -14,18 +21,79 @@ interface PlayerProps {
 const Player = ({
   position, 
   isBusy, 
-  onPlayerMove, 
   onInteract,
   onAttack
 }: PlayerProps) => {
+  const dispatch = useDispatch()
+
+  const keyListening = (e: KeyboardEvent) => {
+    const codePressed = e.code;
+    if (Object.keys(moveKeys).includes(codePressed)) {
+      // Move
+      let [x, y] = moveKeys[codePressed];
+      move({x, y});
+    } else if (codePressed === 'KeyE') {
+      // Interact
+      onInteract!()
+    }
+  }
+
+  const mouseListening = (e: MouseEvent) => {
+    // Attack in different sides
+    const [playerX, playerY] = getAbsolutePosition(position)
+    const [a, b] = [e.clientX - playerX, e.clientY - playerY]
+    const c = Math.sqrt(a*a + b*b)
+    const [x, y] = [a / c * symbolSize, b / c * symbolSize]
+    if (e.button === 0 && e.type==="mousedown") {
+      onAttack!(true, {x, y})
+    } else if (e.button === 0 && e.type==="mouseup"){
+      onAttack!(false, {x, y})
+    }
+  }
+  
+  useEffect(() => {
+    if (isBusy) {
+      return
+    }
+    document.addEventListener("keydown", keyListening);
+    document.addEventListener("mousedown", mouseListening);
+    document.addEventListener("mouseup", mouseListening);
+    return () => {
+      document.removeEventListener("keydown", keyListening);
+      document.removeEventListener("mousedown", mouseListening);
+      document.removeEventListener("mouseup", mouseListening);
+    }
+  })
+
+  const collideCheck = ({x, y}: Position) => {
+    const differentMapsCheck = (mapType: Map<string, { type: string }>) => {
+      let nextBlockType = mapType.get(`${x},${y}`)?.type
+      if (typeof nextBlockType === 'undefined') {
+        return false
+      }
+      return ['w', 'n', 'm'].includes(nextBlockType) ? true : false
+    }
+    return differentMapsCheck(gameMap) || differentMapsCheck(gameNpcs)
+  }
+
+  const move = ({x, y}: Position) => {
+    if (isBusy) {
+      return
+    }
+    const newPosition = {
+      x: position.x + x,
+      y: position.y + y,
+    }
+    if (!collideCheck(newPosition)) {
+      dispatch(positionChange(newPosition))
+    }
+  }
+
   return (
     <Character
       position={ position }
       isBusy={ isBusy }
       isNpc={ false }
-      onCharacterChange={ ({x, y}) => onPlayerMove({x, y}) }
-      onInteract={ onInteract }
-      onAttack={ (isAttack, {x, y}) => onAttack(isAttack, {x, y}) }
     />
   )  
 };
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Attack from '@components/Attack/Attack';
@@ -19,7 +19,6 @@ import { getAbsolutePosition } from '@utils/getAbsolutePosition';
 import { getRelativePosition } from '@utils/getRelativePosition';
 
 import {
-  positionChange,
   busyChange,
   attackChange,
   eyeDirectionChange
@@ -48,17 +47,6 @@ const Map = () => {
   useEffect(() => {
     loadMobs()
   }, [])
-
-  const collideCheck = ({x, y}: Position) => {
-    const differentMapsCheck = (mapType: Map<string, { type: string }>) => {
-      let nextBlockType = mapType.get(`${x},${y}`)?.type
-      if (typeof nextBlockType === 'undefined') {
-        return false
-      }
-      return ['w', 'n', 'm'].includes(nextBlockType) ? true : false
-    }
-    return differentMapsCheck(gameMap) || differentMapsCheck(gameNpcs)
-  };
 
   const interactCheck = () => {
     let {x, y} = hero.position;
@@ -95,19 +83,6 @@ const Map = () => {
     dispatch(attackChange(isAttack))
   }
 
-  const heroPositionChange = ({x, y}: Position) => {
-    if (hero.isBusy) {
-      return
-    }
-    const newPosition = {
-      x: hero.position.x + x,
-      y: hero.position.y + y,
-    }
-    if (!collideCheck(newPosition)) {
-      dispatch(positionChange(newPosition))
-    }
-  };
-
   const renderBlocks = Array.from(gameMap).map(([blockPosition, { type: blockType }]) => {
     let [x, y] = blockPosition.split(',').map(el => parseInt(el));
     return (
@@ -132,7 +107,7 @@ const Map = () => {
       />
     )
   })
-  
+
   const renderMobs = mobs.map((mob: MobState) => {
     const {x, y} = mob.position
     return (
@@ -152,7 +127,7 @@ const Map = () => {
         <Player 
           position={ hero.position }
           isBusy={ hero.isBusy }
-          onPlayerMove={ ({x, y}) => heroPositionChange({x, y}) }
+          // onPlayerMove={ ({x, y}) => heroPositionChange({x, y}) }
           onInteract={ interactCheck }
           onAttack={ (isAttack, {x, y}) => attack(isAttack, {x, y}) }
         />
